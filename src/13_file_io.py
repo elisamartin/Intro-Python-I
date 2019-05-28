1e708f0ed55de095c70c2ad64971c03551c3962a
elisamartin <elisamv93@gmail.com>
2019-05-28 16:43:02 +0200

    MVP

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+foo = open('foo.txt', 'r')
+print(foo.read())
+foo.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+bar = open('bar.txt', 'w')
+
+bar.write('I dream\'d a dream to-night.\n')
+bar.write('And so did I.\n')
+bar.write('Well, what was yours?')
+
+bar.close()
--- src/14_cal.py
+++ src/14_cal.py
@@ -9,11 +9,14 @@ and does the following:
  - If the user doesn't specify any input, your program should 
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
+
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
+
  - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that 
    month and year.
+
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
@@ -22,3 +25,20 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+date = datetime.today()
+
+
+def makeCalendar(month=date.month, year=date.year):
+    month = int(month)
+    year = int(year)
+    cal = calendar.TextCalendar()
+    print(cal.formatmonth(year, month))
+
+
+# print(makeCalendar(8, 1993))
+
+if len(sys.argv) > 3:
+    print('Write max 2 arguments after file name to create new calendar\n')
+else:
+    makeCalendar(*sys.argv[1:])
